f03ecdb77b1240f664409a9a46e199e225806cd3
Szabolcs Berko <szberko@gmail.com>
2019-06-07 09:11:25 +0200

    initial project setup

---
+++ src/main/java/com/symphony/hotelreview/HotelreviewApplication.java
@@ -0,0 +1,13 @@
+package com.symphony.hotelreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelreviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelreviewApplication.class, args);
+	}
+
+}
